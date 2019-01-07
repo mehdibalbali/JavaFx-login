3e49c3fa2a67fb7ef5694e6c28dc21e174657ca6
mehdibalbali <mehdibalbali@yahoo.fr>
2019-01-07 18:32:00 +0100

    creating the project the UI using Jfoenix

---
+++ src/login/Main.java
@@ -0,0 +1,26 @@
+
+package login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/login/view/Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Gestion de Stock ");
+        stage.show();
+    }
+
+   
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
