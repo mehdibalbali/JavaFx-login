ed6e629ab39b051d6a22bf31a8ff888c9b20ffc8
mehdibalbali <mehdibalbali@yahoo.fr>
2019-01-07 18:52:12 +0100

    Create Model.java

---
+++ src/login/model/Model.java
@@ -0,0 +1,35 @@
+package login.model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Model {
+	private final StringProperty login = new SimpleStringProperty();
+	private final StringProperty pwd = new SimpleStringProperty();
+	public final StringProperty loginProperty() {
+		return this.login;
+	}
+	
+	public final String getLogin() {
+		return this.loginProperty().get();
+	}
+	
+	public final void setLogin(final String login) {
+		this.loginProperty().set(login);
+	}
+	
+	public final StringProperty pwdProperty() {
+		return this.pwd;
+	}
+	
+	public final String getPwd() {
+		return this.pwdProperty().get();
+	}
+	
+	public final void setPwd(final String pwd) {
+		this.pwdProperty().set(pwd);
+	}
+	
+	
+	
+}
