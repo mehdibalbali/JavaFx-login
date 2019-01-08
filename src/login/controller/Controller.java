e065f90d17126f4730401609431d6fdb3f2a8814
mehdibalbali <mehdibalbali@yahoo.fr>
2019-01-08 01:02:20 +0100

    Update Controller.java
    change the controller to swich between scenes after logging in

--- src/login/controller/Controller.java
+++ src/login/controller/Controller.java
@@ -1,16 +1,22 @@
 package login.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+
+import java.awt.event.MouseEvent;
+import java.io.IOException;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import login.Implementations;
-import login.model.Model;
+
 
 
 public class Controller {
@@ -21,11 +27,27 @@ public class Controller {
 	    @FXML
 	    private JFXButton btnConnect;
 		Implementations crd = new Implementations();
-	  
-		
-		public void login() {
+		@FXML
+		private Label invalid;
+		@FXML
+		private Parent  root;
+		MouseEvent event ;
+		public void login(ActionEvent event) throws IOException{
 	    	
-	    	crd.log(tfusername.getText(), tfpwd.getText());
-	    }
+	    	
+	    	 
+	    	Parent parent = FXMLLoader.load(getClass().getResource("/login/view/hello.fxml")) ;
+	    	Scene scene =new Scene(parent);
+	    	Stage stage = (Stage)  ( (Node) event.getSource()).getScene().getWindow();
+	    	if (crd.log(tfusername.getText(), tfpwd.getText()))
+            {
+                stage.hide();
+                stage.setScene(scene);
+                stage.show();  
+            }else {
+            	invalid.setText("username and/or password are wrong! ");
+            }
+	   }
+	 }
 		
-}
+
